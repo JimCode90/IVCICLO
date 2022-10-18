43824f023d375d81e2103ba64ee939b416af5517
JimCode90 <jimmypaytan10@gmail.com>
2022-10-18 17:09:48 -0500

    vista detalle de app

--- APLICACIONES_MOVILES_PARA_ANDROID/EP2/EP2Restaurantes/app/src/main/java/com/example/ep2restaurantes/CoffeDetalleActivity.java
+++ APLICACIONES_MOVILES_PARA_ANDROID/EP2/EP2Restaurantes/app/src/main/java/com/example/ep2restaurantes/CoffeDetalleActivity.java
@@ -1,14 +1,84 @@
 package com.example.ep2restaurantes;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.ep2restaurantes.databinding.ActivityCoffeDetalleBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoffeDetalleActivity extends AppCompatActivity {
+
+    private ActivityCoffeDetalleBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coffe_detalle);
+        binding = ActivityCoffeDetalleBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Bundle bundle = getIntent().getExtras();
+        String idtipo = bundle.getString("idtipo");
+        String nombre = bundle.getString("nombre");
+        this.setTitle(nombre);
+        leerDatos(idtipo);
+    }
+
+    private void leerDatos(String idtipo) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://servicioscafe2022.000webhostapp.com/tipoxid.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("DATOS",response);
+                        llenarLista(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("DATOSERROR",error.getMessage());
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<>();
+                map.put("id", idtipo);
+                return map;
+            }
+        };
+        queue.add(stringRequest);
+    }
+
+    private void llenarLista(String response) {
+        System.out.println(response);
+        TextView tvNombre;
+        tvNombre = (TextView) findViewById(R.id.tvNombre);
+        try {
+            JSONArray jsonArray = new JSONArray(response);
+            String nombre= jsonArray.getJSONObject(0).getString("nombre");
+            tvNombre.setText(nombre);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
